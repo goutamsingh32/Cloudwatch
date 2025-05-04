18a44edce076fb7e54524188e3fa2d06966aaa0e
Gomzee <goutamsinghchouhan32@gmail.com>
2025-05-04 15:49:11 +0530

    add express app

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
 
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+const PORT = 5050;
+
+app.use(morgan("combined"));
+
+
+app.get('/user', (req, res)=>{
+    res.status(200).json({message:'success'});
+});
+
+app.listen(PORT, ()=>{
+    console.log('server is running on port:', PORT);
+})
