3d2e0f2c5babacbc5ffb27166e270a92f520a930
Gomzee <goutamsinghchouhan32@gmail.com>
2025-05-04 18:44:00 +0530

    nested function

--- server.js
+++ server.js
@@ -8,8 +8,18 @@ app.use(morgan("combined"));
 
 
 app.get('/user', (req, res)=>{
+    console.log('in users api');
+    insideFunction1();
     res.status(200).json({message:'success'});
 });
+
+function insideFunction1(){
+    console.log('insideFunction 1');
+    insideFunction2();
+}
+function insideFunction2(){
+    console.log('insideFunction 2');
+}
 
 app.listen(PORT, ()=>{
     console.log('server is running on port:', PORT);
